674c504bce117d482f45f6d255671f69acf77d4d
rtdinda <93638490+rtdinda@users.noreply.github.com>
2024-02-28 21:12:45 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showString("Adinda")
+    basic.showNumber(26)
+    basic.showIcon(IconNames.Happy)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+})
