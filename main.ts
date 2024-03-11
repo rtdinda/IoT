9f9c71da5c9578200755375c1736c53b4bf29fbc
rtdinda <93638490+rtdinda@users.noreply.github.com>
2024-03-11 18:23:11 +0800

    practice

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
+input.onGesture(Gesture.Shake, function () {
+    x += 1
+})
+let x = 0
+let list = 0
 basic.forever(function () {
-    basic.showString("Adinda")
-    basic.showNumber(26)
-    basic.showIcon(IconNames.Happy)
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # . . . .
-        # . . . .
-        # # # # #
-        `)
+    basic.showNumber(x)
 })
